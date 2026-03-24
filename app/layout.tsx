deb8aa064f6e51f023db06f75aa750cb24614fdb
Vladimir Korostelev <vladimir_korostelev@MacBook-Air-Vladimir.local>
2026-03-24 07:32:17 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru" className={`${oswald.variable} ${inter.variable}`}>
       <head>
+        {/* Yandex.Metrika */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108210320','ym');
+          ym(108210320,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+        `}} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
@@ -89,6 +99,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         `}} />
       </head>
       <body>
+        <noscript><div><img src="https://mc.yandex.ru/watch/108210320" style={{position:'absolute',left:'-9999px'}} alt="" /></div></noscript>
         <Nav />
         <main>{children}</main>
         <Footer />
