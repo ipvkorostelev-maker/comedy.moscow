b86b446dffbdc6739cea277c4dd00a1fa1bb7bfd
Vladimir Korostelev <vladimir_korostelev@MacBook-Air-Vladimir.local>
2026-03-26 00:05:58 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
           })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108210320', 'ym');
           ym(108210320, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          ym(94210228, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
         `}} />
         <script
           type="application/ld+json"
