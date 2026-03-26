f3ad4d1bbebe7885181d23a7cd66ba13904d0989
Vladimir Korostelev <vladimir_korostelev@MacBook-Air-Vladimir.local>
2026-03-26 09:26:12 +0300

    update

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,63 @@
+import { getAllEvents } from '@/lib/data'
+import { minEventPrice } from '@/lib/utils'
+
+const BASE = 'https://comedy.moscow'
+
+export async function GET() {
+  const events = await getAllEvents()
+
+  const offers = events
+    .map((e) => {
+      const price = minEventPrice(e)
+      const url = `${BASE}/events/${e.slug}`
+      const salesNotes = [
+        e.ageRestriction ? `${e.ageRestriction}+` : '',
+        e.duration ? `Продолжительность: ${e.duration}` : '',
+      ].filter(Boolean).join('. ')
+
+      return `
+    <offer id="${e.id}" available="true">
+      <url><![CDATA[${url}]]></url>
+      <price>${price}</price>
+      <currencyId>RUR</currencyId>
+      <categoryId>1</categoryId>
+      <picture><![CDATA[${e.image}]]></picture>
+      <name><![CDATA[${e.title}]]></name>
+      <description><![CDATA[${e.description ?? e.subtitle ?? e.title}]]></description>
+      ${e.venueName ? `<param name="Место"><![CDATA[${e.venueName}]]></param>` : ''}
+      ${e.city ? `<param name="Город"><![CDATA[${e.city}]]></param>` : ''}
+      <param name="Дата"><![CDATA[${e.date}]]></param>
+      <param name="Время"><![CDATA[${e.time}]]></param>
+      ${e.ageRestriction ? `<param name="Возраст">${e.ageRestriction}+</param>` : ''}
+      ${e.duration ? `<param name="Продолжительность"><![CDATA[${e.duration}]]></param>` : ''}
+      ${salesNotes ? `<sales_notes><![CDATA[${salesNotes}]]></sales_notes>` : ''}
+      ${e.tags?.map((t) => `<param name="Жанр"><![CDATA[${t}]]></param>`).join('\n      ') ?? ''}
+    </offer>`
+    })
+    .join('')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE yml_catalog SYSTEM "shops.dtd">
+<yml_catalog date="${new Date().toISOString().slice(0, 16).replace('T', ' ')}">
+  <shop>
+    <name>Смешно</name>
+    <company>comedy.moscow</company>
+    <url>${BASE}</url>
+    <currencies>
+      <currency id="RUR" rate="1"/>
+    </currencies>
+    <categories>
+      <category id="1">Стендап-концерты</category>
+    </categories>
+    <offers>${offers}
+    </offers>
+  </shop>
+</yml_catalog>`
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  })
+}
