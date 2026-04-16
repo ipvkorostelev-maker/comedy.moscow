17ab54f98fdae0db56d470ea5e208b03bdca352b
Vladimir Korostelev <vladimir_korostelev@MacBook-Air-Vladimir.local>
2026-04-16 09:33:34 +0300

    fix: use standard Metrika tag.js URL, remove ssr:true for both counters

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,9 +82,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108210320', 'ym');
-          ym(108210320, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
-          ym(94359734, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+          ym(108210320, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
+          ym(94359734, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
         `}} />
         <script
           type="application/ld+json"
