84922689d881ce162f94791bf2b68bf31856e8bb
Vladimir Korostelev <vladimir_korostelev@MacBook-Air-Vladimir.local>
2026-05-08 21:41:27 +0300

    perf: defer analytics scripts with next/script afterInteractive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Oswald, Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import Nav from '@/components/layout/Nav'
 import Footer from '@/components/layout/Footer'
@@ -84,45 +85,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        {/* VK Pixel */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          var _tmr = window._tmr || (window._tmr = []);
-          _tmr.push({id: "3764427", type: "pageView", start: (new Date()).getTime()});
-          (function (d, w, id) {
-            if (d.getElementById(id)) return;
-            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-            ts.src = "https://top-fwz1.mail.ru/js/code.js";
-            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-          })(document, window, "topmailru-code");
-        `}} />
-        {/* Yandex.Metrika counter */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
-          ym(108210320, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
-          ym(94359734, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
-        `}} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
         />
-        <script dangerouslySetInnerHTML={{ __html: `
-          var dealerName = 'YandexTicketsDealer';
-          var dealer = window[dealerName] = window[dealerName] || [];
-          dealer.push(['setDefaultClientKey', 'ticketsteam-4063']);
-          dealer.push(['setDefaultRegionId', 213]);
-          (function() {
-            var s = document.createElement('script');
-            s.async = true;
-            s.src = 'https://widget.afisha.yandex.ru/dealer/dealer.js?' + Date.now();
-            document.getElementsByTagName('script')[0].parentNode.insertBefore(s, document.getElementsByTagName('script')[0]);
-          })();
-        `}} />
       </head>
       <body>
         <noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3764427;js=na" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
@@ -132,6 +98,51 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className="pt-20 lg:pt-16">{children}</main>
         <Footer />
         <CookieBanner />
+        <Script
+          id="vk-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: `
+            var _tmr = window._tmr || (window._tmr = []);
+            _tmr.push({id: "3764427", type: "pageView", start: (new Date()).getTime()});
+            (function (d, w, id) {
+              if (d.getElementById(id)) return;
+              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+              ts.src = "https://top-fwz1.mail.ru/js/code.js";
+              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+            })(document, window, "topmailru-code");
+          `}}
+        />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+            ym(108210320, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
+            ym(94359734, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true, trustedDomains:['afisha.yandex.ru','widget.afisha.yandex.ru']});
+          `}}
+        />
+        <Script
+          id="yandex-dealer"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: `
+            var dealerName = 'YandexTicketsDealer';
+            var dealer = window[dealerName] = window[dealerName] || [];
+            dealer.push(['setDefaultClientKey', 'ticketsteam-4063']);
+            dealer.push(['setDefaultRegionId', 213]);
+            (function() {
+              var s = document.createElement('script');
+              s.async = true;
+              s.src = 'https://widget.afisha.yandex.ru/dealer/dealer.js?' + Date.now();
+              document.getElementsByTagName('script')[0].parentNode.insertBefore(s, document.getElementsByTagName('script')[0]);
+            })();
+          `}}
+        />
       </body>
     </html>
   )
